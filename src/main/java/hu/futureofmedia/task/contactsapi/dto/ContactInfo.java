e8a1ed4e2a3fcf209645aea722af6d12735fee76
czpet <czakopeter@freemail.hu>
2021-09-16 19:25:50 +0200

    fix schema info, add no arg constructor, equals, hashCode and toString

--- src/main/java/hu/futureofmedia/task/contactsapi/dto/ContactInfo.java
+++ src/main/java/hu/futureofmedia/task/contactsapi/dto/ContactInfo.java
@@ -3,6 +3,7 @@ package hu.futureofmedia.task.contactsapi.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 public class ContactInfo {
 
@@ -18,7 +19,7 @@ public class ContactInfo {
     @Schema(example = "john.doe@gmail.com")
     private String email;
 
-    @Schema(example = "+36201234567")
+    @Schema(example = "36201234567")
     private String phoneNumber;
 
     @Schema(example = "Company #1")
@@ -28,6 +29,8 @@ public class ContactInfo {
     private String comment;
     private LocalDateTime createdDate;
     private LocalDateTime lastUpdatedDate;
+
+    public ContactInfo() {}
 
     public ContactInfo(Long id, String firstName, String lastName, String email, String phoneNumber, String companyName, String comment, LocalDateTime createdDate, LocalDateTime lastUpdatedDate) {
         this.id = id;
@@ -75,5 +78,33 @@ public class ContactInfo {
 
     public LocalDateTime getLastUpdatedDate() {
         return lastUpdatedDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ContactInfo that = (ContactInfo) o;
+        return id.equals(that.id) && firstName.equals(that.firstName) && lastName.equals(that.lastName) && email.equals(that.email) && Objects.equals(phoneNumber, that.phoneNumber) && companyName.equals(that.companyName) && comment.equals(that.comment) && createdDate.equals(that.createdDate) && lastUpdatedDate.equals(that.lastUpdatedDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, email, phoneNumber, companyName, comment, createdDate, lastUpdatedDate);
+    }
+
+    @Override
+    public String toString() {
+        return "ContactInfo{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", companyName='" + companyName + '\'' +
+                ", comment='" + comment + '\'' +
+                ", createdDate=" + createdDate +
+                ", lastUpdatedDate=" + lastUpdatedDate +
+                '}';
     }
 }
