91f595b140d6324356704f2e5ddea638c9867b2b
czpet <czakopeter@freemail.hu>
2021-09-16 19:18:36 +0200

    Add all args constructor. Implement equals, hashCode, toString.

--- src/main/java/hu/futureofmedia/task/contactsapi/entities/Contact.java
+++ src/main/java/hu/futureofmedia/task/contactsapi/entities/Contact.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Entity
 @EntityListeners(AuditingEntityListener.class)
@@ -22,10 +23,11 @@ public class Contact {
     private String lastName;
     private String email;
     private String phoneNumber;
-    private String comment;
 
     @ManyToOne
     private Company company;
+
+    private String comment;
 
     @Enumerated(EnumType.STRING)
     private Status status;
@@ -39,14 +41,27 @@ public class Contact {
     public Contact() {
     }
 
-    public Contact(String firstName, String lastName, String email, String phoneNumber, String comment, Company company, Status status) {
+    public Contact(String firstName, String lastName, String email, String phoneNumber, Company company, String comment, Status status) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
         this.phoneNumber = phoneNumber;
-        this.comment = comment;
         this.company = company;
+        this.comment = comment;
         this.status = status;
+    }
+
+    public Contact(Long id, String firstName, String lastName, String email, String phoneNumber, Company company, String comment, Status status, LocalDateTime createdDate, LocalDateTime lastModifiedDate) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.company = company;
+        this.comment = comment;
+        this.status = status;
+        this.createdDate = createdDate;
+        this.lastModifiedDate = lastModifiedDate;
     }
 
     public Long getId() {
@@ -109,13 +124,13 @@ public class Contact {
         return this;
     }
 
-    public Contact setComment(String comment) {
-        this.comment = comment;
+    public Contact setCompany(Company company) {
+        this.company = company;
         return this;
     }
 
-    public Contact setCompany(Company company) {
-        this.company = company;
+    public Contact setComment(String comment) {
+        this.comment = comment;
         return this;
     }
 
@@ -126,5 +141,39 @@ public class Contact {
 
     public enum Status {
         ACTIVE, DELETED
+    }
+
+    @PreRemove
+    private void preRemove() {
+        this.status = Status.DELETED;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Contact contact = (Contact) o;
+        return Objects.equals(id, contact.id) && firstName.equals(contact.firstName) && lastName.equals(contact.lastName) && email.equals(contact.email) && Objects.equals(phoneNumber, contact.phoneNumber) && company.equals(contact.company) && comment.equals(contact.comment) && status == contact.status && Objects.equals(createdDate, contact.createdDate) && Objects.equals(lastModifiedDate, contact.lastModifiedDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, email, phoneNumber, company, comment, status, createdDate, lastModifiedDate);
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", company=" + company +
+                ", comment='" + comment + '\'' +
+                ", status=" + status +
+                ", createdDate=" + createdDate +
+                ", lastModifiedDate=" + lastModifiedDate +
+                '}';
     }
 }
