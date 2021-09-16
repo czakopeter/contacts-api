12321ccf07e137b88c9d42c32a38518996ece356
czpet <czakopeter@freemail.hu>
2021-09-16 19:15:37 +0200

    Add findById method

--- src/main/java/hu/futureofmedia/task/contactsapi/repositories/CompanyRepository.java
+++ src/main/java/hu/futureofmedia/task/contactsapi/repositories/CompanyRepository.java
@@ -4,7 +4,10 @@ import hu.futureofmedia.task.contactsapi.entities.Company;
 import org.springframework.data.repository.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CompanyRepository extends Repository<Company, Long> {
     List<Company> findAll();
+
+    Optional<Company> findById(Long id);
 }
