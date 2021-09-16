d488900c436c9d6d1277eee84c21992b027aedcf
czpet <czakopeter@freemail.hu>
2021-09-16 19:28:55 +0200

    fix schema info, add no arg constructor, equals, hashCode and toString

--- src/main/java/hu/futureofmedia/task/contactsapi/dto/ContactMinInfo.java
+++ src/main/java/hu/futureofmedia/task/contactsapi/dto/ContactMinInfo.java
@@ -2,6 +2,8 @@ package hu.futureofmedia.task.contactsapi.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
+import java.util.Objects;
+
 public class ContactMinInfo {
 
     @Schema(example = "1")
@@ -13,18 +15,20 @@ public class ContactMinInfo {
     @Schema(example = "john.doe@gmail.com")
     private String email;
 
-    @Schema(example = "+36201234567")
+    @Schema(example = "36201234567")
     private String phoneNumber;
 
     @Schema(example = "Company #1")
     private String companyName;
 
-    public ContactMinInfo(Long id, String fullName, String companyName, String email, String phoneNumber) {
+    public ContactMinInfo() {}
+
+    public ContactMinInfo(Long id, String fullName, String email, String phoneNumber, String companyName) {
         this.id = id;
         this.fullName = fullName;
-        this.companyName = companyName;
         this.email = email;
         this.phoneNumber = phoneNumber;
+        this.companyName = companyName;
     }
 
     public Long getId() {
@@ -35,15 +39,39 @@ public class ContactMinInfo {
         return fullName;
     }
 
-    public String getCompanyName() {
-        return companyName;
-    }
-
     public String getEmail() {
         return email;
     }
 
     public String getPhoneNumber() {
         return phoneNumber;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ContactMinInfo that = (ContactMinInfo) o;
+        return id.equals(that.id) && fullName.equals(that.fullName) && email.equals(that.email) && Objects.equals(phoneNumber, that.phoneNumber) && companyName.equals(that.companyName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, fullName, email, phoneNumber, companyName);
+    }
+
+    @Override
+    public String toString() {
+        return "ContactMinInfo{" +
+                "id=" + id +
+                ", fullName='" + fullName + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", companyName='" + companyName + '\'' +
+                '}';
     }
 }
