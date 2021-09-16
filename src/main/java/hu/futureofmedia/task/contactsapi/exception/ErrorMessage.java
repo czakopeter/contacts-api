32405760e36a820dcad4bf4177afd696fc7db0db
czpet <czakopeter@freemail.hu>
2021-09-16 19:35:01 +0200

    Rename and add equals and hashCode

--- src/main/java/hu/futureofmedia/task/contactsapi/exception/ErrorMessage.java
+++ src/main/java/hu/futureofmedia/task/contactsapi/exception/ErrorMessage.java
@@ -1,11 +1,13 @@
 package hu.futureofmedia.task.contactsapi.exception;
 
-public class ExceptionMessage {
+import java.util.Objects;
+
+public class ErrorMessage {
 
     private final String field;
     private final String errorMessage;
 
-    public ExceptionMessage(String field, String errorMessage) {
+    public ErrorMessage(String field, String errorMessage) {
         this.field = field;
         this.errorMessage = errorMessage;
     }
@@ -16,6 +18,19 @@ public class ExceptionMessage {
 
     public String getErrorMessage() {
         return errorMessage;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ErrorMessage that = (ErrorMessage) o;
+        return Objects.equals(field, that.field) && Objects.equals(errorMessage, that.errorMessage);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(field, errorMessage);
     }
 
     @Override
